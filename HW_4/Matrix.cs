259a466a6537309cdc6d9605ca7135d12991cc34
Oleksii Kocherhin <Oleksiy.Kochergin@lime-systems.com>
2024-10-15 02:29:04 +0300

    Martix

---
+++ HW_4/Matrix.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_4
+{
+
+	public class Matrix
+	{
+		// Поле для зберігання елементів матриці
+		private double[,] elements;
+
+		// Властивості для отримання кількості рядків і стовпців
+		public int Rows { get; private set; }
+		public int Columns { get; private set; }
+
+		// Конструктор, який ініціалізує матрицю заданих розмірів
+		public Matrix(int rows, int columns)
+		{
+			Rows = rows;
+			Columns = columns;
+			elements = new double[rows, columns];
+		}
+
+		// Індексатор для доступу до елементів матриці
+		public double this[int row, int column]
+		{
+			get { return elements[row, column]; }
+			set { elements[row, column] = value; }
+		}
+
+		// Перевантаження оператора додавання
+		public static Matrix operator +(Matrix a, Matrix b)
+		{
+			if (a.Rows != b.Rows || a.Columns != b.Columns)
+				throw new InvalidOperationException("Матриці повинні бути однакового розміру для додавання!");
+
+			Matrix result = new Matrix(a.Rows, a.Columns);
+			for (int i = 0; i < a.Rows; i++)
+			{
+				for (int j = 0; j < a.Columns; j++)
+				{
+					result[i, j] = a[i, j] + b[i, j];
+				}
+			}
+			return result;
+		}
+
+		// Перевантаження оператора віднімання
+		public static Matrix operator -(Matrix a, Matrix b)
+		{
+			if (a.Rows != b.Rows || a.Columns != b.Columns)
+				throw new InvalidOperationException("Матриці повинні бути однакового розміру для віднімання!");
+
+			Matrix result = new Matrix(a.Rows, a.Columns);
+			for (int i = 0; i < a.Rows; i++)
+			{
+				for (int j = 0; j < a.Columns; j++)
+				{
+					result[i, j] = a[i, j] - b[i, j];
+				}
+			}
+			return result;
+		}
+
+		// Перевантаження оператора множення матриці на матрицю
+		public static Matrix operator *(Matrix a, Matrix b)
+		{
+			if (a.Columns != b.Rows)
+				throw new InvalidOperationException("Кількість стовпців першої матриці повинна дорівнювати кількості рядків другої матриці для множення!");
+
+			Matrix result = new Matrix(a.Rows, b.Columns);
+			for (int i = 0; i < a.Rows; i++)
+			{
+				for (int j = 0; j < b.Columns; j++)
+				{
+					for (int k = 0; k < a.Columns; k++)
+					{
+						result[i, j] += a[i, k] * b[k, j];
+					}
+				}
+			}
+			return result;
+		}
+
+		// Перевантаження оператора множення матриці на число
+		public static Matrix operator *(Matrix a, double scalar)
+		{
+			Matrix result = new Matrix(a.Rows, a.Columns);
+			for (int i = 0; i < a.Rows; i++)
+			{
+				for (int j = 0; j < a.Columns; j++)
+				{
+					result[i, j] = a[i, j] * scalar;
+				}
+			}
+			return result;
+		}
+
+		// Перевантаження оператора рівності
+		public static bool operator ==(Matrix a, Matrix b)
+		{
+			if (a.Rows != b.Rows || a.Columns != b.Columns)
+				return false;
+
+			for (int i = 0; i < a.Rows; i++)
+			{
+				for (int j = 0; j < a.Columns; j++)
+				{
+					if (a[i, j] != b[i, j])
+						return false;
+				}
+			}
+			return true;
+		}
+
+		// Перевантаження оператора нерівності
+		public static bool operator !=(Matrix a, Matrix b)
+		{
+			return !(a == b);
+		}
+
+		// Перевизначений метод Equals
+		public override bool Equals(object obj)
+		{
+			if (obj is Matrix)
+			{
+				return this == (Matrix)obj;
+			}
+			return false;
+		}
+
+		// Перевизначений метод GetHashCode
+		public override int GetHashCode()
+		{
+			return elements.GetHashCode();
+		}
+
+		// Метод для виведення матриці на екран
+		public void Print()
+		{
+			for (int i = 0; i < Rows; i++)
+			{
+				for (int j = 0; j < Columns; j++)
+				{
+					Console.Write(elements[i, j] + "\t");
+				}
+				Console.WriteLine();
+			}
+		}
+	}
+
+}
--- HW_4/Program.cs
+++ HW_4/Program.cs
@@ -1,35 +1,58 @@
 ﻿
 using HW_4;
 
-Employe employe_01 = new Employe(01, "Иванов", "Иван", "ЙТ", "Разработчик", 500);
+//Employe employe_01 = new Employe(01, "Иванов", "Иван", "ЙТ", "Разработчик", 500);
 
-employe_01.EmplInfo();
+//employe_01.EmplInfo();
 
-Employe employe_02 = new Employe(02, "Дужова", "Инна", "Бухгалтерия", "Бухгалтер", 450);
+//Employe employe_02 = new Employe(02, "Дужова", "Инна", "Бухгалтерия", "Бухгалтер", 450);
 
-employe_02.EmplInfo();
+//employe_02.EmplInfo();
 
-employe_01 += 100;
-employe_01.EmplInfo();
+//employe_01 += 100;
+//employe_01.EmplInfo();
 
-Console.WriteLine(employe_01 == employe_02);
-Console.WriteLine(employe_01 > employe_02);
-employe_02 -= 500;
-Console.WriteLine(employe_01.Equals(employe_02));
+//Console.WriteLine(employe_01 == employe_02);
+//Console.WriteLine(employe_01 > employe_02);
+//employe_02 -= 500;
+//Console.WriteLine(employe_01.Equals(employe_02));
 
-// *****************
+//// *****************
 
-City city = new City("Киев", 250000);
-city.CityInfo();
-city += 100;
-city.CityInfo();
+//City city = new City("Киев", 250000);
+//city.CityInfo();
+//city += 100;
+//city.CityInfo();
 
-// *****************
+//// *****************
 
-CreditCard card1 = new CreditCard("UA3489889034834", "2620-4441-4567-3421", 233, 2500);
-card1.CreditCardInfo();
+//CreditCard card1 = new CreditCard("UA3489889034834", "2620-4441-4567-3421", 233, 2500);
+//card1.CreditCardInfo();
 
-CreditCard card2 = new CreditCard("UA538785887345", "2600-3441-5557-1421", 233, 13500);
-card2.CreditCardInfo();
+//CreditCard card2 = new CreditCard("UA538785887345", "2600-3441-5557-1421", 233, 13500);
+//card2.CreditCardInfo();
 
-Console.WriteLine(card1 == card2);
+//Console.WriteLine(card1 == card2);
+
+Matrix matrix1 = new Matrix(2, 2);
+matrix1[0, 0] = 1;
+matrix1[0, 1] = 2;
+matrix1[1, 0] = 3;
+matrix1[1, 1] = 4;
+
+Matrix matrix2 = new Matrix(2, 2);
+matrix2[0, 0] = 5;
+matrix2[0, 1] = 6;
+matrix2[1, 0] = 7;
+matrix2[1, 1] = 8;
+
+Matrix sumMatrix = matrix1 + matrix2;
+Console.WriteLine("Сума матриць:");
+sumMatrix.Print();
+
+Matrix productMatrix = matrix1 * matrix2;
+Console.WriteLine("Добуток матриць:");
+productMatrix.Print();
+
+bool areEqual = matrix1 == matrix2;
+Console.WriteLine("Матриці рівні: " + areEqual);
