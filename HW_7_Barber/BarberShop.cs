2f7499fe7a2ba269e86f360b856c60927ba6e94a
Oleksii Kocherhin <Oleksiy.Kochergin@lime-systems.com>
2024-11-08 02:04:16 +0200

    Add Barder

---
+++ HW_7_Barber/BarberShop.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+class Barbershop
+{
+	private Queue<Customer> waitingRoom = new Queue<Customer>();
+	private readonly int waitingSeats;
+	private bool isBarberSleeping = true;
+	private readonly object lockObject = new object();
+
+	public Barbershop(int seats)
+	{
+		waitingSeats = seats;
+	}
+
+	public void Enter(Customer customer)
+	{
+		lock (lockObject)
+		{
+			if (waitingRoom.Count < waitingSeats)
+			{
+				waitingRoom.Enqueue(customer);
+				Console.WriteLine($"{customer.Name} зайняв місце в очікувальній кімнаті.");
+				Monitor.Pulse(lockObject); // Будимо перукаря
+			}
+			else
+			{
+				Console.WriteLine($"{customer.Name} пішов, оскільки немає вільного місця.");
+			}
+		}
+	}
+
+	public void CutHair()
+	{
+		while (true)
+		{
+			Customer? customer = null;
+
+			lock (lockObject)
+			{
+				while (waitingRoom.Count == 0)
+				{
+					Console.WriteLine("Перукар спить...");
+					isBarberSleeping = true;
+					Monitor.Wait(lockObject); // Чекаємо нових відвідувачів
+				}
+
+				customer = waitingRoom.Dequeue();
+				isBarberSleeping = false;
+				Console.WriteLine($"Перукар починає стрижку {customer.Name}.");
+			}
+
+			// Симуляція процесу стрижки
+			Thread.Sleep(2000);
+			Console.WriteLine($"Перукар закінчив стрижку {customer.Name}.");
+		}
+	}
+}
+
+class Customer
+{
+	public string Name { get; }
+
+	public Customer(string name)
+	{
+		Name = name;
+	}
+}
---
+++ HW_7_Barber/Program.cs
@@ -0,0 +1,27 @@
+﻿//using HW_7_Barber;
+
+
+using System.Runtime.InteropServices;
+
+class Program
+{
+	[DllImport("kernel32.dll")]
+	private static extern bool SetConsoleOutputCP(uint wCodePageID);
+	static void Main(string[] args)
+	{
+		SetConsoleOutputCP(1251);
+
+		Barbershop barbershop = new Barbershop(3); // 3 місця в очікувальній кімнаті
+		Thread barberThread = new Thread(barbershop.CutHair);
+		barberThread.Start();
+
+		for (int i = 1; i <= 10; i++)
+		{
+			Customer customer = new Customer($"Відвідувач {i}");
+			Thread.Sleep(new Random().Next(500, 1000)); // Симуляція часу прибуття відвідувачів
+			barbershop.Enter(customer);
+		}
+
+		barberThread.Join(); // Чекаємо завершення потоку перукаря (необхідно для коректного завершення програми)
+	}
+}
